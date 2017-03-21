e461b2b80d37e5e9d414bd74d3b56d4e6e35be90
Michael Villander <nousvillander@gmail.com>
2017-03-21 18:42:13 -0300

    implements register page and something refactor

---
+++ tests/integration/components/sponsor-list-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('sponsor-list', 'Integration | Component | sponsor list', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{sponsor-list}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#sponsor-list}}
+      template block text
+    {{/sponsor-list}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
